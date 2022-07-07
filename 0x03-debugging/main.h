7387b7b9cbbfede462acc8ab2617a9de725fb511
Kit3l <pchumbakitel@gmail.com>
2022-07-07 13:58:54 -0700

    Task 2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,4 +5,12 @@
 
 void positive_or_negative(int i);
 
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
 #endif /* MAIN_H */
