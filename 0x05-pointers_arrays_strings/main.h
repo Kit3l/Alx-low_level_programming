0ae3df494f005b66af80ffe832afa473b6444ee3
Kit3l <pchumbakitel@gmail.com>
2022-07-12 11:42:47 -0700

    task 1

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,7 +9,24 @@
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ *  Return: On success 1.
+ *  On error, -1 is returned, and errno is set appropriately.
  */
+int _putchar(char c);
 
+/**
+ * reset_to_98 - reset the value of a pointer to 98
+ * @n: integer pointer
+ *
+ * Return: void
+ */
+void reset_to_98(int *n);
+
+/**
+ * swap_int - swap two integers
+ * @a: integer
+ * @b: integer
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b);
