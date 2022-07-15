3800584b6fbce10918a7c69cd3f51c58d94102f3
Kit3l <pchumbakitel@gmail.com>
2022-07-15 01:15:15 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,23 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strcat - concatenate two strings
+ *
+ * Return: char *
+ */
+
+char *_strcat(char *dest, char *src);
+
+/**
+ * _strncat - Entry point
+ * @dest: char pinter
+ * @src: char pointer
+ * @n: integer
+ *
+ * Return: char
+ */
