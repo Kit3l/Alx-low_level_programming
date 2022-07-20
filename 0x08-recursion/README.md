fffe6f0c22fa7f357369e28c5451873d5fb2e8bd
Kit3l <pchumbakitel@gmail.com>
2022-07-20 00:32:49 -0700

    task 0

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+tackling recursions in C
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+int _putchar(char c);
+
+/**
+ * _puts_recursion - print a string using recursion
+ * @s: String to print
+ *
+ * Return: void
+ */
