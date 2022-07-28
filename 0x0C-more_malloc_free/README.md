335e454017e89c23f12deb8c2fd136dd3165fa32
Kit3l <pchumbakitel@gmail.com>
2022-07-28 06:51:06 -0700

    task 0

---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+this is a project on Automatic and dynamic allocation, malloc and free
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,22 @@
+#define _MAIN_H
+
+#define END '\0'
+
+#define EMPTY ""
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <string.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: size of memory to allocate
+ *
+ * Return: void
+ */
+
+void *malloc_checked(unsigned int b);
+
+
