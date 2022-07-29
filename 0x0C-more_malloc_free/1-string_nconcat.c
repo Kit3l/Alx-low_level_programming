d1434e40dbbd79a90f1959e512e4968d7ca527eb
Kit3l <pchumbakitel@gmail.com>
2022-07-29 01:27:27 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,29 +11,32 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *s;
-	int num, len, i, j;
+	char *comb;
+	int i;
+	unsigned int j;
 
-	num = n;
-
-	if (!s1)
-		s1 = EMPTY;
-	if (!s2)
-		s2 = EMPTY;
-	if (num >= (int) strlen(s2))
-		num = strlen(s2);
-
-	len = strlen(s1) + num + 1;
-
-	s = malloc(sizeof(*s) * len);
-	if (!s)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	i = 0;
+	while (s1[i])
+		i++;
+	i++;
+	comb = malloc(sizeof(*comb) * (i + n));
+	if (comb == NULL)
 		return (NULL);
-
-	for (i = 0; s1[i] != END; i++)
-		s[i] = s1[i];
-	for (j = 0; j < num; j++)
-		s[i + j] = s2[j];
-	s[i + j] = END;
-
-	return (s);
+	i = 0;
+	while (s1[i])
+	{
+		comb[i] = s1[i];
+		i++;
+	}
+	for (j = 0; j < n && s2[j] != '\0'; j++)
+	{
+		comb[i] = s2[j];
+		i++;
+	}
+	comb[i] = '\0';
+	return (comb);
 }
