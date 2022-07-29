bd2f3dc4fa53d31280473c1b7b763e45a7204fc3
Kit3l <pchumbakitel@gmail.com>
2022-07-29 04:29:34 -0700

    task 3

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,12 +4,11 @@
  * string_nconcat - allocates memory using malloc
  * @s1: string
  * @s2: string
- * n: length of string
+ * n:length of string
  *
  * Return: pointer to memory location
  */
-
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 {
 	char *s;
 	int i, k;
