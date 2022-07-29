3e3e776be6ee8a19dc35648affb52c6faba162b8
Kit3l <pchumbakitel@gmail.com>
2022-07-29 03:39:32 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,42 +1,38 @@
 #include "main.h"
 
 /**
- * string_nconcat - concatenate s1 and n bytes of s
- * @s1: string 1
- * @s2: string 2
- * @n: n bytes to concat from string 2
+ * string_nconcat - allocates memory using malloc
+ * @s1: string
+ * @s2: string
+ * n: length of string
  *
- * Return: pointer to concatenated string
+ * Return: pointer to memory location
  */
 
-char *string_nconcat(char *s1, char *s2, unsigned int n);
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *comb;
-	int i;
-	unsigned int j;
+	char *s;
+	int i, k;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	i = 0;
-	while (s1[i])
-		i++;
-	i++;
-	comb = malloc(sizeof(*comb) * (i + n));
-	if (comb == NULL)
+	if ((int) n < 0)
 		return (NULL);
-	i = 0;
-	while (s1[i])
-	{
-		comb[i] = s1[i];
-		i++;
-	}
-	for (j = 0; j < n && s2[j] != '\0'; j++)
-	{
-		comb[i] = s2[j];
-		i++;
-	}
-	comb[i] = '\0';
-	return (comb);
+	if (!s1)
+		s1 = EMPTY;
+	if (!s2)
+		s2 = EMPTY;
+
+	s = malloc(sizeof(s1) + (sizeof(char) * (n + 1)));
+	if (!s)
+		return (NULL);
+
+	for (i = 0; s1[i] != END; i++)
+		s[i] = s1[i];
+
+	for (k = 0; s2[k] != END && k < (int) n; k++)
+		s[i + k] = s2[k];
+
+	s[i + k] = END;
+
+	return (s);
 }
+
