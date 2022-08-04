aa6f079816a468ec6d342ad2599e675a32312bf7
Kit3l <pchumbakitel@gmail.com>
2022-08-04 11:43:22 -0700

    task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print numbers
+ * @separator: separator
+ * @n: num of parameters
+ *
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list parameters;
+
+	va_start(parameters, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(parameters, int));
+		if (!separator || i == n - 1)
+			continue;
+
+		printf("%s", separator);
+	}
+
+	va_end(params);
+	printf("\n");
+}
