dbaaebed6a936bf88b0bc19bab8f97a6ad019300
Kit3l <pchumbakitel@gmail.com>
2022-10-26 02:26:32 -0700

    header file

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,11 +1,20 @@
-#ifndef __HASH_DEFS_H__
-#define __HASH_DEFS_H__
+#ifndef __HASH_TABLES_H__
+#define __HASH_TABLES_H__
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define table_t hash_table_t
-#define node_t hash_node_t
-
-#endif
+/**
+ * struct hash_node_s - Node of a hash table
+ * @key: The key, string
+ * The key is unique in the HashTable
+ * @value: The value corresponding to a key
+ * @next: A pointer to the next node of the List
+ */
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
