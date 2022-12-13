903928e492a4fc901aafccbd0bf6103d7c2c7ea2
Kit3l <pchumbakitel@gmail.com>
2022-12-13 00:17:26 -0800

    task 1 advanced correction

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,7 +16,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i;
 	unsigned int j;
 	unsigned int s1_len;
-	unsigned int concat_len
+	unsigned int concat_len;
 	char *concat;
 
 	s1_len = 0;
@@ -31,19 +31,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1_len++;
 
 	concat_len = s1_len + n;
-
-	concat = malloc(sizeof(char) * (concat_len + 1));
-	if (concat == NULL)
-		return (NULL);
-
-	for (i = 0; i < s1_len; i++)
-	{
-		concat[i] = s1[i];
-	}
-	for (j = 0; j < n; j++, i++)
-	{
-		concat[i] = s2[j];
-	}
 
 	concat[i] = '\0';
 
